6a076c11969f3240b36b2cd812aa75c916ca43c3
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2022-12-05 13:24:18 +0100

    Day 5

---
+++ 05/day_05.py
@@ -0,0 +1,57 @@
+import re
+
+stacks_test = [
+    "ZN",
+    "MCD",
+    "P",
+]
+
+stacks_input = [
+    "RNFVLJSM",
+    "PNDZFJWH",
+    "WRCDG",
+    "NBS",
+    "MZWPCBFN",
+    "PRMW",
+    "RTNGLSW",
+    "QTHFNBV",
+    "LMHZNF",
+]
+
+
+def read_file():
+    with open("input.txt") as file:
+        return file.read().strip().split("\n")
+
+
+def parse_line(line):
+    regex = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    res = regex.search(line)
+    return tuple(map(int, res.groups()))
+
+
+def part_1():
+    stacks = list(map(list, stacks_input))
+    lines = read_file()
+    for line in lines:
+        moves, origin, dest = parse_line(line)
+        for _ in range(moves):
+            stacks[dest - 1].append(stacks[origin - 1].pop())
+    res = "".join(map(lambda stack: stack[-1], stacks))
+    print(res)
+
+
+def part_2():
+    stacks = list(map(list, stacks_input))
+    lines = read_file()
+    for line in lines:
+        moves, origin, dest = parse_line(line)
+        to_move = stacks[origin - 1][-moves:]
+        stacks[origin - 1] = stacks[origin - 1][:-moves]
+        stacks[dest - 1] = stacks[dest - 1] + to_move
+    res = "".join(map(lambda stack: stack[-1], stacks))
+    print(res)
+
+
+# part_1()
+# part_2()
