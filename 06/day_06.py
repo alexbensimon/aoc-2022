c6eeb9157fcf75b83f5bfb4e48f667c0695e3974
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2022-12-06 11:56:37 +0100

    Day 6

---
+++ 06/day_06.py
@@ -0,0 +1,30 @@
+def read_file():
+    with open("input.txt") as file:
+        return file.read().strip()
+
+
+def is_marker(chars):
+    return len(chars) == len(set(chars))
+
+
+def find_marker(size):
+    chars = list(read_file())
+    index = size
+    while index < len(chars):
+        size_chars = chars[index - size : index]
+        if is_marker(size_chars):
+            print(index)
+            break
+        index += 1
+
+
+def part_1():
+    find_marker(4)
+
+
+def part_2():
+    find_marker(14)
+
+
+# part_1()
+# part_2()
